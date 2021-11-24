1292fd26e25629017fe619c512d08edf5047eb29
adr_0 <andr-krs.o@yandex.ru>
2021-11-24 19:22:34 +0700

    itog v3
    -правки js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    let burger = document.querySelector('.header__burger');
-    let menu = document.querySelector('.header__menu');
-    let body = document.querySelector('body');
-    let header = document.querySelector('.header');
+    const burger = document.querySelector('.header__burger');
+    const menu = document.querySelector('.header__menu');
+    const body = document.querySelector('body');
+    const header = document.querySelector('.header');
 
     burger.addEventListener('click',() => {
         burger.classList.toggle('active')
         menu.classList.toggle('active')
         body.classList.toggle('lock')
         if(header.classList.contains('header_scroll')){
-            header.classList.remove('header_scroll');
-        }
-        else{
-            header.classList.add('header_scroll');
+            header.classList.toggle('header_scroll');
         }
     })
 })
 
 window.addEventListener('scroll', function() {
 
-    let header = document.querySelector('.header');
+    const header = document.querySelector('.header');
     if (window.pageYOffset > 10) {header.classList.add('header_scroll');}
     else {header.classList.remove('header_scroll');}
 });
@@ -28,6 +25,6 @@ window.addEventListener('scroll', function() {
 let scroll = document.querySelector('#scroll');
 
 scroll.addEventListener('click',() => {
-    let attractions = document.querySelector('#attractions');
+    const attractions = document.querySelector('#attractions');
     attractions.scrollIntoView({behavior: "smooth"});
 });
